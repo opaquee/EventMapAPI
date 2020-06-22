406ec340a81d647b0fd31822aab3dda7ca7b92a9
JoshGuempel <joshguempel5@gmail.com>
2020-06-22 00:33:09 -0500

    fix app connection to postgres

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.14
 
 require (
 	github.com/99designs/gqlgen v0.11.3
+	github.com/jinzhu/gorm v1.9.14 // indirect
+	github.com/lib/pq v1.7.0 // indirect
 	github.com/vektah/gqlparser/v2 v2.0.1
 )
--- server.go
+++ server.go
@@ -1,25 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/opaquee/EventMapAPI/graph"
 	"github.com/opaquee/EventMapAPI/graph/generated"
 )
 
 const defaultPort = "8080"
 
+var db *gorm.DB
+
 func main() {
+	var err error
+	db, err := gorm.Open("postgres", "host=db port=5432 dbname=postgres user=user password=secret sslmode=disable")
+	defer db.Close()
+	if err != nil {
+		fmt.Println(err)
+		panic("failed to connect database")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{
+		DB: db,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
