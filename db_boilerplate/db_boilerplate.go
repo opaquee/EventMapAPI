063628656c4f2cedef6d78b62817b1bc055c6f25
JoshGuempel <joshguempel5@gmail.com>
2020-06-23 20:54:52 -0500

    remove db_boilerplate package, add documentaion to main

--- db_boilerplate/db_boilerplate.go
+++
@@ -1,9 +0,0 @@
-package db_boilerplate
-
-import (
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-)
-
-func migrateDB(db *DB) {
-	db.AutoMigrate(&User{}, &Event{})
-}
--- server.go
+++ server.go
@@ -12,6 +12,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/opaquee/EventMapAPI/graph"
 	"github.com/opaquee/EventMapAPI/graph/generated"
+	"github.com/opaquee/EventMapAPI/graph/model"
 )
 
 const defaultPort = "8080"
@@ -20,6 +21,8 @@ var db *gorm.DB
 
 func main() {
 	var err error
+
+	log.Println("Connecting to database...")
 	db, err := gorm.Open("postgres", "host=db port=5432 dbname=postgres user=user password=secret sslmode=disable")
 	defer db.Close()
 	if err != nil {
@@ -27,6 +30,10 @@ func main() {
 		panic("failed to connect database")
 	}
 
+	log.Println("Migrating tables...")
+	db.AutoMigrate(&model.User{}, &model.Event{})
+
+	log.Println("Starting server. Hold on to your potatoes!")
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
